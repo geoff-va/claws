b76df91e836edb2bba847798dec20f96d05eb7f0
Geoff Von Allmen <geoff@ibleducation.com>
2023-03-02 08:54:47 -0800

    Add conf_reader_factory

--- src/claws/conf_readers.py
+++ src/claws/conf_readers.py
@@ -1,8 +1,12 @@
+"""Functions for reading configuration files. They should return a dict-like object"""
 import json
 from pathlib import Path
+from typing import Callable
 
 import tomlkit as tk
 from dotenv import dotenv_values
+
+from .exceptions import UnsupportedFileTypeError
 
 
 def read_env(path: Path) -> dict:
@@ -20,3 +24,18 @@ def read_toml(path: Path) -> tk.TOMLDocument:
     """Return a dict-like object from reading a .toml formatted file"""
     with path.open("rb") as fin:
         return tk.load(fin)
+
+
+_CONF_READER_MAP = {
+    ".env": read_env,
+    ".json": read_json,
+    ".toml": read_toml,
+}
+
+
+def conf_reader_factory(file_ext: str) -> Callable[[Path], dict]:
+    """Return a function to read the specified file_ext"""
+    reader = _CONF_READER_MAP.get(file_ext)
+    if reader is None:
+        raise UnsupportedFileTypeError(f"Unsupported config file type: {file_ext}")
+    return reader
