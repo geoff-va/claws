24692a5fb72847a5b0dba6e9951ba825d5ccb802
Geoff Von Allmen <geoff@ibleducation.com>
2023-03-01 21:49:16 -0800

    _validate_setup on __init__

--- src/claws/validation/base_validator.py
+++ src/claws/validation/base_validator.py
@@ -1,12 +1,14 @@
-import re
 from typing import Any
 
 
 class BaseValidator:
     """Base Validator class"""
 
+    def __init__(self) -> None:
+        self._validate_setup()
+
     def _validate_setup(self) -> None:
         """Raise ImproperlyConfiguredError if there are validation setup errors"""
 
     def check(self, value: Any) -> None:
-        self._validate_setup()
+        """Raise ValidationError if necessary"""
