924307cae90590e6973e78e5aaa74b4e9d630594
Geoff Von Allmen <geoff@ibleducation.com>
2023-03-01 20:43:13 -0800

    Log loaded env file path

--- src/claws/env.py
+++ src/claws/env.py
@@ -1,8 +1,12 @@
+import logging
 from pathlib import Path
 
 from dotenv import dotenv_values
+
+log = logging.getLogger(__name__)
 
 
 class EnvReader:
     def __init__(self, env_path: Path) -> None:
         self.data = dotenv_values(env_path)
+        log.info("Loaded %s", env_path)
